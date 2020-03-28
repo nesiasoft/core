6d23cbaa320bd53e5e5707d25940e8448adda8a5
R. Kukuh <rkukuh@gmail.com>
2020-03-28 09:39:36 +0700

    Prepare to publish both migration and config files

--- src/CoreServiceProvider.php
+++ src/CoreServiceProvider.php
@@ -11,36 +11,17 @@ class CoreServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'core');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'core');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('core.php'),
+                __DIR__.'/../config/comment.php' => config_path('comment.php'),
             ], 'config');
 
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/core'),
-            ], 'views');*/
 
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/core'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/core'),
-            ], 'lang');*/
-
-            // Registering package commands.
-            // $this->commands([]);
+            if (! class_exists('CreateCommentsTable')) {
+                $this->publishes([
+                    __DIR__.'/../database/migrations/create_comments_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_comments_table.php'),
+                ], 'migrations');
+            }
         }
     }
 
@@ -49,10 +30,8 @@ class CoreServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'core');
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'comment');
 
-        // Register the main class to use with the facade
         $this->app->singleton('core', function () {
             return new Core;
         });
