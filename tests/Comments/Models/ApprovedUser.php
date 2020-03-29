58ac393ae1aa8201e973ffeb21267f708f83e78e
R. Kukuh <rkukuh@gmail.com>
2020-03-29 11:33:29 +0700

    Use imported class

--- tests/Comments/Models/ApprovedUser.php
+++ tests/Comments/Models/ApprovedUser.php
@@ -3,18 +3,14 @@
 namespace Nesiasoft\Core\Tests\Comments\Models;
 
 use Illuminate\Foundation\Auth\User;
+use Illuminate\Database\Eloquent\Model;
 use Nesiasoft\Core\Comments\Contracts\Commentator;
 
 class ApprovedUser extends User implements Commentator
 {
     protected $table = 'users';
 
-    /**
-     * Check if a comment for a specific model needs to be approved.
-     * @param mixed $model
-     * @return bool
-     */
-    public function needsCommentApproval($model): bool
+    public function needsCommentApproval(Model $model): bool
     {
         return false;
     }
