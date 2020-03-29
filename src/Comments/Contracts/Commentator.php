fed12ece7e1bf3f66da790ddfb68f3d1db32badd
R. Kukuh <rkukuh@gmail.com>
2020-03-29 11:32:44 +0700

    Use imported class

--- src/Comments/Contracts/Commentator.php
+++ src/Comments/Contracts/Commentator.php
@@ -2,12 +2,14 @@
 
 namespace Nesiasoft\Core\Comments\Contracts;
 
+use Illuminate\Database\Eloquent\Model;
+
 interface Commentator
 {
     /**
      * Check if a comment for a specific model needs to be approved.
-     * @param mixed $model
+     * @param Model $model
      * @return bool
      */
-    public function needsCommentApproval($model): bool;
+    public function needsCommentApproval(Model $model): bool;
 }
